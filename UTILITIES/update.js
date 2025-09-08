8a23e93e8bf23bb2fe8e9c0f8b676e31ea357768
Arcadia-diddy <lexeewammera@gmail.com>
2025-09-08 06:06:52 -0500

    Delete update.js

--- UTILITIES/update.js
+++
@@ -1,8 +0,0 @@
-window.onblur = () => {
-  document.title = "Google Docs";
-  document.getElementById("favicon").href = "https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_document_x16.png";
-};
-window.onfocus = () => {
-  document.title = "ArcadiaX";
-  document.getElementById("favicon").href = "favicon-32x32.png";
-};
