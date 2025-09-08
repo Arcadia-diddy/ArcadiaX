9676a81d6d22dd2d7fc8c0bee68ed51b4b55e347
Arcadia-diddy <lexeewammera@gmail.com>
2025-09-07 23:40:35 -0500

    ok

--- UTILITIES/update.js
+++ UTILITIES/update.js
@@ -1,9 +1,8 @@
 window.onblur = () => {
   document.title = "Google Docs";
-  document.getElementById("favicon").href =
-    "https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_document_x16.png";
+  document.getElementById("favicon").href = "https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_document_x16.png";
 };
 window.onfocus = () => {
   document.title = "ArcadiaX";
-  document.getElementById("favicon").href = "favicon-32x32.png";
+  document.getElementById("favicon").href = "UTILITIES\favicon-32x32.png";
 };
